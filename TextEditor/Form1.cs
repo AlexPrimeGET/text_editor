75efad9e059604a2d8b9f4de6ebfa670720e6f24
AlexPrime <minecraftevolutionteam@gmail.com>
2020-05-05 23:28:03 +0300

    new file:   TextEditor.sln 	new file:   TextEditor/App.config 	new file:   TextEditor/Form1.Designer.cs 	new file:   TextEditor/Form1.cs 	new file:   TextEditor/Form1.resx 	new file:   TextEditor/Form2.Designer.cs 	new file:   TextEditor/Form2.cs 	new file:   TextEditor/Form2.resx 	new file:   TextEditor/Program.cs 	new file:   TextEditor/Properties/AssemblyInfo.cs 	new file:   TextEditor/Properties/Resources.Designer.cs 	new file:   TextEditor/Properties/Resources.resx 	new file:   TextEditor/Properties/Settings.Designer.cs 	new file:   TextEditor/Properties/Settings.settings 	new file:   "TextEditor/Resources/icons8-\320\262\321\201\321\202\320\260\320\262\320\270\321\202\321\214-32.png" 	new file:   "TextEditor/Resources/icons8-\320\262\321\213\320\261\320\276\321\200-\321\210\321\200\320\270\321\204\321\202\320\260-32.png" 	new file:   "TextEditor/Resources/icons8-\320\262\321\213\321\200\320\265\320\267\320\260\321\202\321\214-32.png" 	new file:   "TextEditor/Resources/icons8-\320\264\320\276\320\261\320\260\320\262\320\270\321\202\321\214-\321\204\320\260\320\271\320\273-32.png" 	new file:   "TextEditor/Resources/icons8-\320\264\320\276\320\272\321\203\320\274\320\265\320\275\321\202-32.png" 	new file:   "TextEditor/Resources/icons8-\320\264\320\276\320\272\321\203\320\274\320\265\320\275\321\202.svg" 	new file:   "TextEditor/Resources/icons8-\320\275\320\276\320\262\320\260\321\217-\320\272\320\276\320\277\320\270\321\217-32.png" 	new file:   "TextEditor/Resources/icons8-\320\275\320\276\320\262\321\213\320\271-\321\204\320\260\320\271\320\273-32.png" 	new file:   "TextEditor/Resources/icons8-\320\276\321\202\320\272\321\200\321\213\321\202\321\214-\320\277\320\260\320\277\320\272\321\203-32.png" 	new file:   "TextEditor/Resources/icons8-\320\276\321\202\320\272\321\200\321\213\321\202\321\214-\320\277\320\260\320\277\320\272\321\203.svg" 	new file:   "TextEditor/Resources/icons8-\320\276\321\202\320\274\320\265\320\275\320\270\321\202\321\214-32.png" 	new file:   "TextEditor/Resources/icons8-\320\277\320\276\320\262\321\202\320\276\321\200\320\270\321\202\321\214-32.png" 	new file:   "TextEditor/Resources/icons8-\321\200\320\265\320\264\320\260\320\272\321\202\320\270\321\200\320\276\320\262\320\260\320\275\320\270\320\265-\321\204\320\260\320\271\320\273\320\260-32.png" 	new file:   "TextEditor/Resources/icons8-\321\201\320\272\320\276\320\277\320\270\321\200\320\276\320\262\320\260\321\202\321\214-32.png" 	new file:   "TextEditor/Resources/icons8-\321\203\320\262\320\265\320\273\320\270\321\207\320\270\321\202\321\214-\321\210\321\200\320\270\321\204\321\202-32.png" 	new file:   "TextEditor/Resources/icons8-\321\203\320\274\320\265\320\275\321\214\321\210\320\270\321\202\321\214-\321\210\321\200\320\270\321\204\321\202-32.png" 	new file:   "TextEditor/Resources/icons8-\321\204\320\260\320\271\320\273-32.png" 	new file:   "TextEditor/Resources/icons8-\321\204\320\260\320\271\320\273.svg" 	new file:   "TextEditor/Resources/icons8-\321\204\320\276\321\200\320\274\320\260\321\202\320\270\321\200\320\276\320\262\320\260\320\275\320\270\320\265-\321\201\321\202\320\270\320\273\321\217-\321\210\321\200\320\270\321\204\321\202\320\260-32.png" 	new file:   "TextEditor/Resources/icons8-\321\206\320\262\320\265\321\202-\320\267\320\260\320\273\320\270\320\262\320\272\320\270-32.png" 	new file:   "TextEditor/Resources/icons8-\321\206\320\262\320\265\321\202-\321\202\320\265\320\272\321\201\321\202\320\260-32.png" 	new file:   TextEditor/Resources/te_tsb_time.ico 	new file:   TextEditor/Resources/wordpad.ico 	new file:   "TextEditor/Resources/\321\201\320\260\320\274\321\213\320\271\321\201\321\207\320\260\321\201\321\202\320\273\320\270\320\262\321\213\320\271\320\272\320\276\321\200\320\265\320\265\321\206.png" 	new file:   TextEditor/TextEditor.csproj 	new file:   TextEditor/wordpad.ico 	modified:   TextEditor/Form1.Designer.cs 	modified:   TextEditor/Form1.cs

---
+++ TextEditor/Form1.cs
@@ -0,0 +1,415 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextEditor
+{
+    public partial class te_form : Form
+    {
+
+        const string TITLE = "Текстовый редактор";
+        const string QUANTITY = "Сим ";
+        const string PATH = "Путь: ";
+
+
+        public te_form(string FileName)
+        {
+            InitializeComponent();
+            if (FileName.Length > 0)
+            {
+                string[] path = FileName.Split('\\');
+
+                this.Text = TITLE + $" || {path[path.Length - 1]}";
+
+                te_sb_path.Text = PATH + FileName;
+
+                te_rtb_editor.Text = File.ReadAllText(FileName, Encoding.Default);
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            te_sb_quantity.Text = QUANTITY + 0.ToString();
+
+            te_menu_newFile.Click += Te_menu_newFile_Click;
+
+            te_tsb_newFile.Click += Te_menu_newFile_Click;
+
+            te_menu_saveFile.Click += Te_menu_saveFile_Click;
+
+            te_tsb_save.Click += Te_menu_saveFile_Click;
+
+            te_menu_openFile.Click += Te_menu_openFile_Click;
+
+            te_tsb_open.Click += Te_menu_openFile_Click;
+
+            te_menu_closeFile.Click += Te_menu_closeFile_Click;
+
+            te_menu_saveAs.Click += Te_menu_saveAs_Click;
+
+            te_rtb_editor.TextChanged += Te_rtb_editor_TextChanged;
+
+            te_rtb_editor.SelectionChanged += Te_rtb_editor_SelectionChanged;
+
+            te_menu_copy.Click += Te_menu_copy_Click;
+            te_tsb_copy.Click += Te_menu_copy_Click;
+            te_cm_copy.Click += Te_menu_copy_Click;
+
+            te_menu_cut.Click += Te_menu_cut_Click;
+            te_tsb_cut.Click += Te_menu_cut_Click;
+            te_cm_cut.Click += Te_menu_cut_Click;
+
+            te_menu_insert.Click += Te_menu_insert_Click;
+            te_tsb_insert.Click += Te_menu_insert_Click;
+            te_cm_insert.Click += Te_menu_insert_Click;
+
+            te_menu_undo.Click += Te_menu_undo_Click;
+            te_tsb_undo.Click += Te_menu_undo_Click;
+            te_cm_undo.Click += Te_menu_undo_Click;
+
+            te_menu_redo.Click += Te_menu_redo_Click;
+            te_tsb_redo.Click += Te_menu_redo_Click;
+            te_cm_redo.Click += Te_menu_redo_Click;
+
+            te_menu_selectAll.Click += Te_menu_selectAll_Click;
+
+            te_menu_font.Click += Te_menu_font_Click;
+            te_tsb_font.Click += Te_menu_font_Click; ;
+
+            te_menu_fontColor.Click += Te_menu_fontColor_Click;
+            te_tsb_fontColor.Click += Te_menu_fontColor_Click;
+
+            te_menu_backgroundColor.Click += Te_menu_backColor_Click;
+            te_tsb_backColor.Click += Te_menu_backColor_Click;
+
+            te_menu_exit.Click += Te_menu_exit_Click;
+
+            this.Width = Properties.Settings.Default.formWidth;
+            this.Height = Properties.Settings.Default.formHeight;
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.formWidth = this.Width;
+            Properties.Settings.Default.formHeight = this.Height;
+            base.OnFormClosing(e);
+        }
+
+        /// Завершение работы
+
+        private void Te_menu_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        /// Изменение цвета фона
+
+        private void Te_menu_backColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                te_rtb_editor.BackColor = cd.Color;
+            }
+        }
+
+        
+        /// Изменение цвета шрифта
+        
+        private void Te_menu_fontColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                te_rtb_editor.ForeColor = cd.Color;
+            }
+        }
+
+        
+        /// Именение шрифта в редакторе
+        
+        private void Te_menu_font_Click(object sender, EventArgs e)
+        {
+            FontDialog fd = new FontDialog();
+
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                te_rtb_editor.Font = fd.Font;
+            }
+        }
+
+        
+        /// Выбрать все в окне редактора
+        private void Te_menu_selectAll_Click(object sender, EventArgs e)
+        {
+            te_rtb_editor.Focus();
+            te_rtb_editor.SelectAll();
+        }
+
+        
+        /// Вернуть предыдущее действие 
+        
+        private void Te_menu_redo_Click(object sender, EventArgs e)
+        {
+            if (te_rtb_editor.CanRedo == true)
+            {
+                if (te_rtb_editor.RedoActionName != "Delete")
+                {
+                    te_rtb_editor.Redo();
+                }
+            }
+        }
+
+        
+        /// Отменить предыдущее действие
+        
+        private void Te_menu_undo_Click(object sender, EventArgs e)
+        {
+            if (te_rtb_editor.CanUndo == true)
+            {
+                te_rtb_editor.Undo();
+                te_rtb_editor.ClearUndo();
+            }
+        }
+
+        
+        /// Вставить скопированный фрагмент
+        
+        private void Te_menu_insert_Click(object sender, EventArgs e)
+        {
+            te_rtb_editor.Paste();
+        }
+
+        
+        /// Вырезать выделенный фрагмент
+        
+        private void Te_menu_cut_Click(object sender, EventArgs e)
+        {
+            if (te_rtb_editor.SelectionLength > 0)
+            {
+                te_rtb_editor.Cut();
+            }
+        }
+
+       
+        /// Копирование выделенного фрагмента
+        
+        private void Te_menu_copy_Click(object sender, EventArgs e)
+        {
+            if (te_rtb_editor.SelectionLength > 0)
+            { 
+                te_rtb_editor.Copy();
+            }
+        }
+
+        
+        /// Обработка измениения выделения в редакторе
+        
+        private void Te_rtb_editor_SelectionChanged(object sender, EventArgs e)
+        {
+            if (te_rtb_editor.SelectionLength > 0)
+            {
+                te_menu_copy.Enabled = true;
+                te_menu_cut.Enabled = true;
+                te_tsb_copy.Enabled = true;
+                te_tsb_cut.Enabled = true;
+                te_cm_copy.Enabled = true;
+                te_cm_cut.Enabled = true;
+            }
+            else
+            {
+                te_menu_copy.Enabled = false;
+                te_menu_cut.Enabled = false;
+                te_tsb_copy.Enabled = false;
+                te_tsb_cut.Enabled = false;
+                te_cm_copy.Enabled = false;
+                te_cm_cut.Enabled = false;
+            }
+            te_sb_row.Text = "Стр " + (te_rtb_editor.GetLineFromCharIndex(te_rtb_editor.SelectionStart) + 1);
+            te_sb_col.Text = "Стлб " + (te_rtb_editor.SelectionStart - te_rtb_editor.GetFirstCharIndexOfCurrentLine());
+        }
+
+        
+        /// Сохранить файл как
+        
+        private void Te_menu_saveAs_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.Filter = "Все файлы(*.*)|*.*|Текстовые файлы(*txt)|*.txt";
+            sfd.FilterIndex = 2;
+            sfd.Title = "Тестовый редактор || Сохранить файл как ...";
+            sfd.FileName = "file";              //начальное наименование файла
+            sfd.DefaultExt = "txt";                 //задаем начальное раширение файла
+            sfd.ValidateNames = true;               //валидация введенного имени
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                string[] path = sfd.FileName.Split('\\');
+
+                this.Text = TITLE + $" || {path[path.Length - 1]}";
+
+                te_sb_path.Text = PATH + sfd.FileName;
+
+                File.WriteAllText(sfd.FileName, te_rtb_editor.Text);
+
+                EnableDisableElements(true);
+            }
+        }
+
+        
+        /// Закрытие файла
+        
+        private void Te_menu_closeFile_Click(object sender, EventArgs e)
+        {
+            te_rtb_editor.Clear();
+            EnableDisableElements(false);
+        }
+
+        
+        /// Открытие файла
+        
+        private void Te_menu_openFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.Filter = "Все файлы(*.*)|*.*|Текстовые файлы(*txt)|*.txt";
+            ofd.FilterIndex = 2;
+            ofd.Title = "Тестовый редактор :: Открытие файла";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                te_rtb_editor.Clear();
+
+                string[] path = ofd.FileName.Split('\\');
+
+                this.Text = TITLE + $" :: {path[path.Length - 1]}";
+
+                te_sb_path.Text = PATH + ofd.FileName;
+
+                te_rtb_editor.Text = File.ReadAllText(ofd.FileName);
+
+                EnableDisableElements(true);
+            }
+        }
+
+
+        /// Подсчет символов в редактора
+        
+        private void Te_rtb_editor_TextChanged(object sender, EventArgs e)
+        {
+            te_sb_quantity.Text = QUANTITY +  te_rtb_editor.TextLength.ToString();
+            
+        }
+
+
+        /// Сохранение файла
+
+        private void Te_menu_saveFile_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(te_sb_path.Text.Substring(6), te_rtb_editor.Text);
+        }
+
+        
+        /// Создание нового файла
+        
+        private void Te_menu_newFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.Filter = "Все файлы(*.*)|*.*|Текстовые файлы(*txt)|*.txt";
+            sfd.FilterIndex = 2;
+            sfd.Title = "Тестовый редактор :: Создание новго файла";
+            sfd.FileName = "file";              //начальное наименование файла
+            sfd.DefaultExt = "txt";                 //начальное расширение файла
+            sfd.ValidateNames = true;               //валидация введенного имени
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                te_rtb_editor.Clear();
+                string[] path = sfd.FileName.Split('\\');
+
+                this.Text = TITLE + $" :: {path[path.Length - 1]}";
+
+                te_sb_path.Text = PATH + sfd.FileName;
+
+                StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.Default);
+                sw.Write("");
+                sw.Close();
+
+                EnableDisableElements(true);
+
+            }
+        }
+
+        
+        /// Включение/выключение элементов
+        
+        private void EnableDisableElements(bool status)
+        {
+            te_menu_saveFile.Enabled = status;
+            te_tsb_save.Enabled = status;
+
+            if (!status)
+            {
+                te_sb_path.Text = PATH;
+                this.Text = TITLE;
+            }
+        }
+
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            
+        }
+
+        /// Справка
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 te_license = new Form2();
+            te_license.Show();
+        }
+
+        private void файлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// Увеличение шрифта
+        private void te_tsb_fontEnlarge_Click(object sender, EventArgs e)
+        {
+            if (te_rtb_editor.Font.Size < 300F)
+                te_rtb_editor.Font = new Font(te_rtb_editor.Font.Name, te_rtb_editor.Font.Size+2F, te_rtb_editor.Font.Style);
+        }
+
+        /// Уменьшение шрифта
+        private void te_tsb_fontReduce_Click(object sender, EventArgs e)
+        {
+            if (te_rtb_editor.Font.Size > 2F)
+            te_rtb_editor.Font = new Font(te_rtb_editor.Font.Name, te_rtb_editor.Font.Size - 2F, te_rtb_editor.Font.Style);
+        }
+
+        private void te_form_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.formWidth = this.Width;
+            Properties.Settings.Default.formHeight = this.Height;
+            Properties.Settings.Default.Save();
+        }
+
+        private void строкаСостоянияToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            строкаСостоянияToolStripMenuItem.Checked = !строкаСостоянияToolStripMenuItem.Checked;
+            te_statusBar.Visible = !te_statusBar.Visible;
+        }
+    }
+}
---
+++ TextEditor/Form2.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace TextEditor
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.richTextBox1.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.richTextBox1.Location = new System.Drawing.Point(303, 16);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox1.Size = new System.Drawing.Size(276, 286);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "Текстовый редактор\nДубель Александр\nГруппа 2191121\n\nВсе права принадлежат всем лю" +
+    "дям мира.\n\nПродукт не лицензирован.";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TextEditor.Properties.Resources.самыйсчастливыйкореец;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(279, 286);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 331);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 11F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form2";
+            this.Text = "Текстовый редактор: сведения";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
